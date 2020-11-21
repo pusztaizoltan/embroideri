5d64374625fc4cd95a106e014a2e6a3b409eb233
pusztaizoltan <a.pusztaizoltan@gmail.com>
2020-11-21 22:01:47 +0100

    add drawTriangle overloaded solution

--- src/main/java/com/codecool/embroidery/Embroidery.java
+++ src/main/java/com/codecool/embroidery/Embroidery.java
@@ -4,12 +4,14 @@ import java.util.Arrays;
 
 public class Embroidery {
     public static void main(String[] args) {
-        int[][] rectangle0 = drawRectangle(5, 3);
-        System.out.print("Default rectangle:\n");
-        printShape(rectangle0);
-        System.out.print("\n");
-        int[][] rectangle = drawRectangle(8, 6, 2, 1, 2);
-        printShape(rectangle);
+//        int[][] rectangle0 = drawRectangle(5, 3);
+//        System.out.print("Default rectangle:\n");
+//        printShape(rectangle0);
+//        System.out.print("\n");
+//        int[][] rectangle = drawRectangle(8, 6, 2, 1, 2);
+//        printShape(rectangle);
+        int[][] triangle = drawTriangle(6, 1, 3);
+        printShape(triangle);
     }
     private static int[][] drawRectangle(int width, int height){
         return drawRectangle(width, height, 0, 1, 0);
@@ -54,8 +56,22 @@ public class Embroidery {
         return rectangle;
     }
 
+    private static int[][] drawTriangle(int height){
+        return drawTriangle(height, 1, 1);
+    }
     private static int[][] drawTriangle(int height, Integer borderColor, Integer fillColor) {
-        return new int[0][0];
+        int[][] triangle = new int[height][height*2-1];
+        for (int i = height-1; i >= 0; i--) {
+            for (int j = 0; j < triangle[0].length; j++) {
+                if(j < height - 1 -i || j> triangle[0].length -height + i){
+                    triangle[i][j] = 0;
+                } else if (j < height -i || j> triangle[0].length -height -1 + i || i == height-1){
+                    triangle[i][j] = borderColor;
+                } else {
+                    triangle[i][j] = fillColor;}
+            }
+        }
+        return triangle;
     }
 
     private static int[][] drawChristmasTree(int blocks, Integer borderColor, Integer fillColor) {
